a1a11f1f6897427736d3b3a99a89b334e754ad52
Stone-WYD <1174872613@qq.com>
2023-11-28 11:19:47 +0800

    调整文件结构

--- src/main/java/com/wyd/reactorweb/config/ReactWebConfig.java
+++ src/main/java/com/wyd/reactorweb/config/ReactWebConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Import;
 
 /**
  * @program: reactor_web
- * @description: 考虑到一些内容后期要封装成 spring-boot-starter 项目使用，此处使用一个配置类作为 starter 入口
+ * @description: 考虑到一些内容后期要封装成 spring-boot-starter 项目使用，此处使用一个配置类作为 starter 入口，并且在此引入其他配置类
  * @author: Stone
  * @create: 2023-11-13 18:19
  **/
